695a8e3facc8f2ad5bd626e5491430b9927e7619
LGomees <lucasgomesjk@gmail.com>
2024-11-24 18:23:07 -0300

    Creating dim_location destination verification

--- etl/ingest.py
+++ etl/ingest.py
@@ -25,44 +25,91 @@ datasource_df = raw_df["datasource"].drop_duplicates()
 datasource_df = datasource_df.rename("datasource").to_frame()
 
 # Updating dim_datasource
-def insert_into_dim_datasource(engine, df):
-    with engine.connect() as conn:
-        for _, row in df.iterrows():
-            datasource_value = row["datasource"]
+# def insert_into_dim_datasource(engine, df):
+#     with engine.connect() as conn:
+#         for _, row in df.iterrows():
+#             datasource_value = row["datasource"]
 
-            # Check if 'source_name' already exists in the database
-            check_query = text("""  
-                SELECT COUNT(*) FROM dim_datasource WHERE source_name = :source_name
-            """)
-            result = conn.execute(check_query, {"source_name": datasource_value}).fetchone()
+#             # Check if 'source_name' already exists in the database
+#             check_query = text("""  
+#                 SELECT COUNT(*) FROM dim_datasource WHERE source_name = :source_name
+#             """)
+#             result = conn.execute(check_query, {"source_name": datasource_value}).fetchone()
 
-            if result[0] == 0:
-                try:
-                    insert_query = text("""
-                        INSERT INTO dim_datasource (source_name)
-                        VALUES (:source_name)
-                        RETURNING id
-                    """)
-                    result = conn.execute(insert_query, {"source_name": datasource_value})
+#             if result[0] == 0:
+#                 try:
+#                     insert_query = text("""
+#                         INSERT INTO dim_datasource (source_name)
+#                         VALUES (:source_name)
+#                         RETURNING id
+#                     """)
+#                     result = conn.execute(insert_query, {"source_name": datasource_value})
 
-                    conn.commit()
+#                     conn.commit()
 
-                    print(f"Registry '{datasource_value}' inserted.")
-                except Exception as e:
-                    print(f"Error during query execution to '{datasource_value}': {e}")
-            else:
-                print(f"Registry '{datasource_value}' already exists.")
+#                     print(f"Registry '{datasource_value}' inserted.")
+#                 except Exception as e:
+#                     print(f"Error during query execution to '{datasource_value}': {e}")
+#             else:
+#                 print(f"Registry '{datasource_value}' already exists.")
 
-insert_into_dim_datasource(engine, datasource_df)
+# insert_into_dim_datasource(engine, datasource_df)
 
-## DIM_LOCATION
-location_df = raw_df[["origin_coord", "region"]].drop_duplicates()
+# ## DIM_LOCATION
+# ### Looking to origin coordinates
+# location_origin_df = raw_df[["origin_coord", "region"]].drop_duplicates()
+
+# # Separating long and lat to be inserted in the database
+# location_origin_df[['longitude', 'latitude']] = location_origin_df['origin_coord'].str.extract(r'POINT \(([-\d.]+) ([-\d.]+)\)')
+
+# # Updating dim_location (origin)
+# def insert_into_dim_location(engine, df):
+#     with engine.connect() as conn:
+#         for _, row in df.iterrows():
+#             longitude_value = row["longitude"]
+#             latitude_value = row["latitude"]
+#             region_value = row["region"]
+
+#             # Check if the location (longitude, latitude) already exists in the database
+#             check_query = text("""
+#                 SELECT COUNT(*) FROM dim_location 
+#                 WHERE longitude = :longitude AND latitude = :latitude
+#             """)
+#             result = conn.execute(check_query, {
+#                 "longitude": longitude_value,
+#                 "latitude": latitude_value
+#             }).fetchone()
+
+#             if result[0] == 0:
+#                 # If the location does not exist, insert it
+#                 try:
+#                     insert_query = text("""
+#                         INSERT INTO dim_location (longitude, latitude, region)
+#                         VALUES (:longitude, :latitude, :region)
+#                     """)
+#                     result = conn.execute(insert_query, {
+#                         "longitude": longitude_value,
+#                         "latitude": latitude_value,
+#                         "region": region_value
+#                     })
+
+#                     # Commit the transaction (not necessary for the current context, SQLAlchemy will commit automatically)
+#                     conn.commit()
+#                 except Exception as e:
+#                     print(f"Error during query execution for {longitude_value}, {latitude_value}, {region_value}: {e}")
+#             else:
+#                 # If the location already exists, skip insertion
+#                 print(f"Registry with Longitude {longitude_value}, Latitude {latitude_value}, and Region '{region_value}' already exists.")
+
+# insert_into_dim_location(engine, location_origin_df)
+
+### Looking to destination coordinates
+location_destination_df = raw_df[["destination_coord", "region"]].drop_duplicates()
 
 # Separating long and lat to be inserted in the database
-location_df[['longitude', 'latitude']] = location_df['origin_coord'].str.extract(r'POINT \(([-\d.]+) ([-\d.]+)\)')
+location_destination_df[['longitude', 'latitude']] = location_destination_df['destination_coord'].str.extract(r'POINT \(([-\d.]+) ([-\d.]+)\)')
 
-# Updating dim_location
-def insert_into_dim_location(engine, df):
+def verify_destination_dim_location(engine, df):
     with engine.connect() as conn:
         for _, row in df.iterrows():
             longitude_value = row["longitude"]
@@ -100,4 +147,4 @@ def insert_into_dim_location(engine, df):
                 # If the location already exists, skip insertion
                 print(f"Registry with Longitude {longitude_value}, Latitude {latitude_value}, and Region '{region_value}' already exists.")
 
-insert_into_dim_location(engine, location_df)
+verify_destination_dim_location(engine, location_destination_df)
